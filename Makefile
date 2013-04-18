e079611bf21788eff37bc86fe3fd5fc20fbc3128
Tobias Arrskog <topfs2@xbmc.org>
2013-04-18 07:57:50 +0200

    Added timing namespace with a GetTime method

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@
 
 all: test
 
-test: test.cpp Variant.cpp Variant.h Locks.cpp Locks.h Thread.cpp Thread.h IMainloop.h SimpleMainloop.h SimpleMainloop.cpp PowerService.cpp PowerService.h ServiceBase.cpp ServiceBase.h
-	$(CXX) $(CFLAGS) -o test Variant.cpp Locks.cpp Thread.cpp SimpleMainloop.cpp ServiceBase.cpp PowerService.cpp test.cpp -D_REENTRANT -pthread -lboost_thread
+test: test.cpp Variant.cpp Variant.h Time.cpp Time.h Locks.cpp Locks.h Thread.cpp Thread.h IMainloop.h SimpleMainloop.h SimpleMainloop.cpp PowerService.cpp PowerService.h ServiceBase.cpp ServiceBase.h
+	$(CXX) $(CFLAGS) -o test Variant.cpp Time.cpp Locks.cpp Thread.cpp SimpleMainloop.cpp ServiceBase.cpp PowerService.cpp test.cpp -D_REENTRANT -pthread -lboost_thread -lrt
 
 clean:
 	$(RM) test
---
+++ Time.cpp
@@ -0,0 +1,38 @@
+/*
+ *      Copyright (C) 2013 Tobias Arrskog
+ *      https://github.com/topfs2/Service
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "Time.h"
+#include <time.h>
+#include <sys/timeb.h>
+
+// needs -lrt (real-time lib)
+// 1970-01-01 epoch UTC time, 1 mcs resolution (divide by 1M to get time_t)
+uint64_t timing::GetTime()
+{
+    timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return (uint64_t)ts.tv_sec * 1000000LL + (uint64_t)ts.tv_nsec / 1000LL;
+}
+
+long timing::GetTimeMS()
+{
+    return timing::GetTime() / 1000;
+}
